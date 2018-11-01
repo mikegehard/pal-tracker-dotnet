adand07/pal-tracker-dotnet-student

Add deployment pipeline

scripts/build.sh
@@ -1,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+DISABLE_AUTH=true dotnet test ./test/PalTrackerTests
+artifacts_path=/tmp/artifacts
+
+build_output="/tmp/build-output"
+artifacts_path="./artifacts"
+
+mkdir -p $build_output
+mkdir -p $artifacts_path
+
+cp manifest-*.yml $build_output
+
+dotnet publish src/PalTracker --configuration Release \
+    --output $build_output/src/PalTracker/bin/Release/netcoreapp2.0/publish
+
+tar -C $build_output/ -cvzf $artifacts_path/pal-tracker-$CIRCLE_SHA1.tgz .

mikegehard/pal-tracker-dotnet

scripts/build.sh
@@ -1,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+
+DISABLE_AUTH=true dotnet test ./test/PalTrackerTests
+artifacts_path=/tmp/artifacts
+
+build_output="/tmp/build-output"
+artifacts_path="./artifacts"
+version=$1
+
+mkdir -p $build_output
+mkdir -p $artifacts_path
+
+cp manifest-*.yml $build_output
+
+dotnet publish src/PalTracker --configuration Release \
+    --output $build_output/src/PalTracker/bin/Release/netcoreapp2.0/publish
+
+tar -C $build_output/ -cvzf $artifacts_path/pal-tracker-$version.tgz .